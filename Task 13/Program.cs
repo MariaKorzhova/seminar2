7d167505492cf9e1f89824eede44603845d13dd2
Maria Korzhova <mari.shaidullova@bk.ru>
2022-12-05 17:22:16 +0300

    Task 13

---
+++ Task 13/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите число");
+string num = Convert.ToString(Console.ReadLine());
+if (num.Length >= 3)
+{
+    System.Console.WriteLine(num[2]);
+}
+else{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+
