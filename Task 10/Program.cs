a8dd4a642b825f204cfde1297f897182a8e95eab
Maria Korzhova <mari.shaidullova@bk.ru>
2022-12-05 17:09:54 +0300

    Task 10

---
+++ Task 10/Program.cs
@@ -0,0 +1,10 @@
+﻿System.Console.WriteLine("Введите трёхзначное число");
+string num = Convert.ToString(Console.ReadLine());
+if (num.Length == 3)
+{
+    System.Console.WriteLine(num[1]);
+}
+else{
+    System.Console.WriteLine("Нужно ввести трёхзначное число");
+}
+
