9da81e2099f8e799042e9ef503e447f672f4c3d4
Maria Korzhova <mari.shaidullova@bk.ru>
2022-12-05 18:33:49 +0300

    Task 15

---
+++ Task 15/Program.cs
@@ -0,0 +1,35 @@
+﻿
+/*
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+switch (num){
+    case 1:
+        Console.WriteLine("Нет");
+        break;
+    case 2:
+        Console.WriteLine("Нет");
+        break;
+    case 3:
+        Console.WriteLine("Нет");
+        break;
+    case 4:
+        Console.WriteLine("Нет");
+        break;
+    case 5:
+        Console.WriteLine("Нет");
+        break;
+    case 6:
+        Console.WriteLine("Да");
+        break;
+    case 7:
+        Console.WriteLine("Да");
+        break;
+    default:
+        Console.WriteLine("Число не соответствует какому-либо дню недели");
+        break;
+} */
+
+System.Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string result = (num < 8 && (num == 6 || num == 7))? "Да" : "Число не соответствует какому-либо дню недели";
+System.Console.WriteLine(result);
